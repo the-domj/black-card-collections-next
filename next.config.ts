db721aee4f82ff2d1796e927f7946b4038103954
the-domj <76676505+the-domj@users.noreply.github.com>
2026-03-17 01:14:20 -0700

    Fix CSS import order and ESLint config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,24 @@
 import type { NextConfig } from "next";
+import userConfig from './clouduser.next.config';
+
+const webflowOverrides: NextConfig = {
+  basePath: '/app',
+  assetPrefix: 'https://83bbddbd-6647-4791-bd09-2e1ac6f3df3f.wf-app-prod.cosmic.webflow.services/app',
+  images: {
+    ...userConfig.images,
+    // TODO: determine whether any of the non-custom loader options (imgix, cloudinary, akamai) work
+    // and if so allow them to be used here
+    loader: 'custom',
+    loaderFile: userConfig.images?.loaderFile || './webflow-loader.ts',
+  },
+};
 
 const nextConfig: NextConfig = {
-  basePath: "/app",
+  ...userConfig,
+  ...webflowOverrides,
 };
 
 export default nextConfig;
-// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 initOpenNextCloudflareForDev();
